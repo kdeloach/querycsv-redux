c482c1d839cdc9915a6c46cfb71fc77ced79410e
Kevin DeLoach <kdeloach@gmail.com>
2019-02-24 13:50:18 -0500

    Revert "Require Python >=2 and <3"
    This reverts commit 59f9cce6fb6aecad933dc79928ed1c02b43f86e1.

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from distutils.core import setup
 
 setup(name='querycsv-redux',
       version='4.2.0',
-      python_requires='>=2, <3',
       description="Execute SQL code against data contained in one or more "
                   "comma-separated-value (CSV) files.",
       author='Dreas Nielsen',
