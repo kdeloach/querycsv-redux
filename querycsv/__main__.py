f6ae2197b14eaed7283918878a41179be2075c6e
Kevin DeLoach <kdeloach@gmail.com>
2019-02-24 13:46:04 -0500

    Support running as package
    Example:
    ```
    $ python -m querycsv -h
    ```

--- querycsv/__main__.py
+++ querycsv/__main__.py
@@ -1,15 +1,6 @@
-import os
+#!/usr/bin/env python
 import sys
 
-# If we are running from a wheel, add the wheel to sys.path
-if __package__ == '':
-    # first dirname call strips of '/__main__.py', second strips off '/package'
-    # Resulting path is the name of the wheel itself
-    # Add that to sys.path so we can import package
-    path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, path)
-
-from querycsv.querycsv import main
-
 if __name__ == '__main__':
+    from .querycsv import main
     sys.exit(main())
